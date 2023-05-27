2ecb3d11c9e9679f86ed4fdc3be9a29617a63357
diuca puig <diucapuig@gmail.com>
2023-05-27 18:52:20 +0000

    mised changes

--- src/js/views/add.js
+++ src/js/views/add.js
@@ -11,98 +11,95 @@ const Add = () => {
     setInfo({ ...info, [event.target.id]: event.target.value });
     console.log(info);
   };
+
   useEffect(() => {
     if (params.id != 0) {
       actions.contact_current_getinator(params.id);
     }
   }, []);
-  useEffect(() => {
-    let current = store.current;
-    console.log("currentinadd");
-    console.log(current);
-  }, [store.current]);
 
-  return (
-    <>
-      <div className="mt-5 container ">
-        <form
-          className="my-5"
-          onSubmit={(event) => {
-            event.preventDefault();
-            if (params.id != 0) {
-              actions.contact_updatinator(params.id, info);
-            } else {
-              actions.contact_adinator(info);
-            }
-          }}
-        >
-          <div className="mb-3">
-            <label htmlFor="full_name" className="form-label">
-              Full Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="full_name"
-              aria-describedby="namelHelp"
-              placeholder="Full Name"
-              required
-              onChange={contact_setinator}
-              defaultValue={
-                params.id != 0 && typeof current !== "undefined"
-                  ? current.full_name
-                  : "asd"
+  if (store.current != null) {
+    const current = store.current;
+    return (
+      <>
+        <div className="mt-5 container ">
+          <form
+            className="my-5"
+            onSubmit={(event) => {
+              event.preventDefault();
+              if (params.id != 0) {
+                actions.contact_updatinator(params.id, info);
+              } else {
+                actions.contact_adinator(info);
               }
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-              required
-              onChange={contact_setinator}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="phone" className="form-label">
-              Phone
-            </label>
-            <input
-              type="number"
-              className="form-control"
-              id="phone"
-              placeholder="Enter phone"
-              required
-              onChange={contact_setinator}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="address" className="form-label">
-              Adress
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="address"
-              placeholder="Enter Address"
-              required
-              onChange={contact_setinator}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary add_btn">
-            Save
-          </button>
-          <Link to={"/"}>or get back to contacts</Link>
-        </form>
-      </div>
-    </>
-  );
+            }}
+          >
+            <div className="mb-3">
+              <label htmlFor="full_name" className="form-label">
+                Full Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="full_name"
+                aria-describedby="namelHelp"
+                placeholder="Full Name"
+                required
+                onChange={contact_setinator}
+                defaultValue={params.id != 0 ? current.full_name : "asd"}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+                required
+                onChange={contact_setinator}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="phone" className="form-label">
+                Phone
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                id="phone"
+                placeholder="Enter phone"
+                required
+                onChange={contact_setinator}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="address" className="form-label">
+                Adress
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="address"
+                placeholder="Enter Address"
+                required
+                onChange={contact_setinator}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary add_btn">
+              Save
+            </button>
+            <Link to={"/"}>or get back to contacts</Link>
+          </form>
+        </div>
+      </>
+    );
+  } else {
+    return "Loading";
+  }
 };
 
 export default Add;
